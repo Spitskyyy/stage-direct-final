71343d927c0de1f74a5cbc97ffa1326dc1247d32
Spitskyyy <grall.emeric@ik.me>
2025-03-03 14:42:02 +0100

    finalisation envoie mail

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -42,7 +42,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('grall.emeric@ik.me', 'StageDirect'))
+                    ->from(new Address('emeric.grall@ik.me', 'StageDirect'))
                     ->to((string) $user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
