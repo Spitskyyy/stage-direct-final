2f9fa80ddee4849f00c23bb3751015041209dbd4
Spitskyyy <grall.emeric@ik.me>
2025-03-12 09:29:55 +0100

    finalisatation internship

--- src/Controller/InternshipController.php
+++ src/Controller/InternshipController.php
@@ -15,13 +15,37 @@ use Symfony\Component\Routing\Attribute\Route;
 final class InternshipController extends AbstractController
 {
     #[Route(name: 'app_internship_index', methods: ['GET'])]
-    public function index(InternshipRepository $internshipRepository): Response
+    public function index(Request $request, InternshipRepository $internshipRepository): Response
     {
+        $limit = 10; // Nombre d'éléments par page
+        $page = (int) $request->query->get('page', 1); // Page actuelle, par défaut 1
+        $searchTerm = $request->query->get('search', ''); // Paramètre de recherche, par défaut ''
+
+        $criteria = [];
+        // Récupération du nombre total d'éléments (en tenant compte du filtre de recherche)
+        $totalRecords = $internshipRepository->count($criteria);
+        $totalPages = (int) ceil($totalRecords / $limit); // Nombre total de pages
+
+        if (empty($searchTerm)) {
+            // Calcul de l'offset pour la pagination
+            $offset = ($page - 1) * $limit;
+            $internships = $internshipRepository->findBy([], null, $limit, $offset);
+        } else {
+            if ($searchTerm) {
+                $criteria['name'] = $searchTerm; // Exemple de critère
+            }
+            $internships = $internshipRepository->searchBy($criteria);
+        }
+        
         return $this->render('internship/index.html.twig', [
-            'internships' => $internshipRepository->findAll(),
+            'internships' => $internships,
+            'total_records' => $totalRecords,
+            'total_pages' => $totalPages,
+            'current_page' => $page,
+            'search_term' => $searchTerm, // Passer le terme de recherche au template
         ]);
     }
-
+   
     #[Route('/new', name: 'app_internship_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
