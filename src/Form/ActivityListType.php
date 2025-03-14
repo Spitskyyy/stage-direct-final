992e812b96344bee5380393261d498cf44692702
Spitskyyy <grall.emeric@ik.me>
2025-03-14 12:55:00 +0100

    test

--- src/Form/ActivityListType.php
+++ src/Form/ActivityListType.php
@@ -8,21 +8,25 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ActivityListType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+
+
         $builder
-            ->add('contained')
+            ->add('contained', TextareaType::class, [
+                'attr' => ['class' => 'tinymce'],
+            ])
             ->add('is_verified')
             ->add('internship', EntityType::class, [
                 'class' => Internship::class,
                 'choice_label' => 'id',
-            ])
-        ;
-    }
+            ]);
 
+    }
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
