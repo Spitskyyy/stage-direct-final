0b8878b1b3c8bebf2b6019d9a128f4e4cf28f0b3
LAVEILLE Mathis <lavemat@ndlp.fr>
2025-03-28 16:47:14 +0100

    Ajout de la fonction denyAccessUnlessVerified()

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Company;
+use App\Entity\User;
 use App\Form\CompanyType;
 use App\Repository\CompanyRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -11,21 +12,27 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Doctrine\ORM\Tools\Pagination\Paginator;
-// use Symfony\Component\Security\Http\Attribute\IsGranted;
-
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use Symfony\Component\HttpFoundation\StreamedResponse;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 #[Route('/company')]
 final class CompanyController extends AbstractController
 {
+    private function denyAccessUnlessVerified(User $user): void
+    {
+        if (!$user->isVerified()) {
+            throw new AccessDeniedException('Vous devez vérifier votre compte pour accéder à cette page.');
+        }
+    }
+
     #[Route(name: 'app_company_index', methods: ['GET'])]
     #[IsGranted('ROLE_USER')]
-    // #[IsGranted('ROLE_STUDENT')]
-    public function index(Request $request, EntityManagerInterface $entityManager)
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $this->denyAccessUnlessVerified($this->getUser()); // Vérification ici
+
         $queryBuilder = $entityManager->getRepository(Company::class)->createQueryBuilder('c');
 
         for ($i = 1; $i <= 3; $i++) {
@@ -33,15 +40,11 @@ final class CompanyController extends AbstractController
             $searchField = $request->query->get('search_field' . $i);
 
             if ($search && $searchField) {
-                // Construction dynamique du filtre
                 $queryBuilder
                     ->andWhere("c.$searchField LIKE :search$i")
                     ->setParameter("search$i", '%' . $search . '%');
             }
         }
-
-        // Debug de la requête générée
-        dump($queryBuilder->getQuery()->getSQL());
 
         $companies = $queryBuilder->getQuery()->getResult();
 
@@ -56,12 +59,12 @@ final class CompanyController extends AbstractController
         ]);
     }
 
-
     #[Route('/new', name: 'app_company_new', methods: ['GET', 'POST'])]
     #[IsGranted('ROLE_USER')]
-    // #[IsGranted('ROLE_STUDENT')]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $this->denyAccessUnlessVerified($this->getUser());
+
         $company = new Company();
         $form = $this->createForm(CompanyType::class, $company);
         $form->handleRequest($request);
@@ -70,7 +73,7 @@ final class CompanyController extends AbstractController
             $entityManager->persist($company);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_company_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_company_index');
         }
 
         return $this->render('company/new.html.twig', [
@@ -79,27 +82,30 @@ final class CompanyController extends AbstractController
         ]);
     }
 
-    #[Route('/company/{id}', name: 'app_company_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'app_company_show', methods: ['GET'])]
     #[IsGranted('ROLE_USER')]
-    // #[IsGranted('ROLE_STUDENT')]
     public function show(Company $company): Response
     {
+        $this->denyAccessUnlessVerified($this->getUser());
+
         return $this->render('company/show.html.twig', [
             'company' => $company,
         ]);
     }
 
-    #[Route('/company/{id}/edit', name: 'app_company_edit', methods: ['GET', 'POST'])]
-    // #[IsGranted('ROLE_STUDENT')]
+    #[Route('/{id}/edit', name: 'app_company_edit', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_USER')]
     public function edit(Request $request, Company $company, EntityManagerInterface $entityManager): Response
     {
+        $this->denyAccessUnlessVerified($this->getUser());
+
         $form = $this->createForm(CompanyType::class, $company);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_company_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_company_index');
         }
 
         return $this->render('company/edit.html.twig', [
@@ -108,44 +114,41 @@ final class CompanyController extends AbstractController
         ]);
     }
 
-    #[Route(path: '/company/{id}', name: 'app_company_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'app_company_delete', methods: ['POST'])]
     #[IsGranted('ROLE_USER')]
-    // #[IsGranted('ROLE_STUDENT')]
     public function delete(Request $request, Company $company, EntityManagerInterface $entityManager): Response
     {
+        $this->denyAccessUnlessVerified($this->getUser());
+
         if ($this->isCsrfTokenValid('delete' . $company->getId(), $request->getPayload()->getString('_token'))) {
             $entityManager->remove($company);
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_company_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_company_index');
     }
 
     #[Route('/export', name: 'app_company_export', methods: ['GET'])]
     #[IsGranted('ROLE_USER')]
-    // #[IsGranted('ROLE_STUDENT')]
     public function exportToExcel(CompanyRepository $companyRepository): StreamedResponse
     {
-        // Récupérer toutes les entreprises
+        $this->denyAccessUnlessVerified($this->getUser());
+
         $companies = $companyRepository->findAll();
 
-        // Créer une réponse "streamed" pour générer le fichier dynamiquement
         $response = new StreamedResponse(function () use ($companies) {
             $spreadsheet = new Spreadsheet();
             $sheet = $spreadsheet->getActiveSheet();
 
-            // Ajouter les en-têtes
             $sheet->setCellValue('A1', 'Id');
             $sheet->setCellValue('B1', 'Nom');
-            $sheet->setCellValue('C1', 'Addresse');
+            $sheet->setCellValue('C1', 'Adresse');
             $sheet->setCellValue('D1', 'Ville');
             $sheet->setCellValue('E1', 'Code postal');
             $sheet->setCellValue('F1', 'Pays');
-            $sheet->setCellValue('F1', 'Téléphone');
-            $sheet->setCellValue('F1', 'Email');
+            $sheet->setCellValue('G1', 'Téléphone');
+            $sheet->setCellValue('H1', 'Email');
 
-
-            // Ajouter les données des entreprises
             $row = 2;
             foreach ($companies as $company) {
                 $sheet->setCellValue('A' . $row, $company->getId());
@@ -156,20 +159,15 @@ final class CompanyController extends AbstractController
                 $sheet->setCellValue('F' . $row, $company->getCountry());
                 $sheet->setCellValue('G' . $row, $company->getPhone());
                 $sheet->setCellValue('H' . $row, $company->getEmail());
-                $sheet->setCellValue('H' . $row, $company->isVerified());
                 $row++;
             }
 
-            // Créer l'objet Writer
             $writer = new Xlsx($spreadsheet);
-
-            // Sauvegarder dans php://output
             $writer->save('php://output');
         });
 
-        // Configurer les en-têtes de la réponse
         $response->headers->set('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        $response->headers->set('Content-Disposition', 'attachment;filename="companies_export.xlsx"');
+        $response->headers->set('Content-Disposition', 'attachment;filename="companies.xlsx"');
         $response->headers->set('Cache-Control', 'max-age=0');
 
         return $response;
