5c9db89c4a89778f0ee6347bb6769d9ea7d8c059
MathisLaveille <mathislaveille@ik.me>
2025-04-28 00:36:49 +0200

    moi

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -14,36 +14,73 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/user')]
 final class UserController extends AbstractController
 {
-    #[Route(name: 'app_user_index', methods: ['GET'])]
+    #[Route('/', name: 'app_user_index', methods: ['GET'])]
     #[IsGranted('ROLE_MODERATOR')]
-    public function index(Request $request, UserRepository $userRepository): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         $limit = 10;
-        $page = (int) $request->query->get('page', 1);
-        $searchTerm = $request->query->get('search', '');
-
-        $criteria = [
-            'isVerified' => true,
-            'is_verified_user' => true,
-        ];
-
-        if (!empty($searchTerm)) {
-            // Adaptable selon la recherche voulue
-            $criteria['lastname'] = $searchTerm;
-        }
-
-        $totalRecords = $userRepository->count($criteria);
-        $totalPages = (int) ceil($totalRecords / $limit);
+        $page = max(1, $request->query->getInt('page', 1));
         $offset = ($page - 1) * $limit;
 
-        $users = $userRepository->findBy($criteria, null, $limit, $offset);
+        // Récupération des paramètres de recherche
+        $searchTerms = $request->query->all('search');
+        $searchFields = $request->query->all('search_field');
+
+        // Construction de la requête
+        $queryBuilder = $entityManager->getRepository(User::class)->createQueryBuilder('u')
+            ->where('1=1');
+
+        // Application des filtres de recherche
+        if (!empty($searchTerms) && !empty($searchFields)) {
+            foreach ($searchTerms as $index => $term) {
+                if (!empty($term) && isset($searchFields[$index])) {
+                    switch ($searchFields[$index]) {
+                        case 'email':
+                            $queryBuilder->andWhere('u.email LIKE :email' . $index)
+                                ->setParameter('email' . $index, "%{$term}%");
+                            break;
+                        case 'firstname':
+                            $queryBuilder->andWhere('u.firstname LIKE :firstname' . $index)
+                                ->setParameter('firstname' . $index, "%{$term}%");
+                            break;
+                        case 'lastname':
+                            $queryBuilder->andWhere('u.lastname LIKE :lastname' . $index)
+                                ->setParameter('lastname' . $index, "%{$term}%");
+                            break;
+                        case 'grade':
+                            $queryBuilder->andWhere('u.grade LIKE :grade' . $index)
+                                ->setParameter('grade' . $index, "%{$term}%");
+                            break;
+                        case 'speciality':
+                            $queryBuilder->andWhere('u.speciality LIKE :speciality' . $index)
+                                ->setParameter('speciality' . $index, "%{$term}%");
+                            break;
+                        case 'isVerified':
+                            $isVerified = strtolower($term) === 'yes' || $term === '1';
+                            $queryBuilder->andWhere('u.isVerified = :isVerified' . $index)
+                                ->setParameter('isVerified' . $index, $isVerified);
+                            break;
+                    }
+                }
+            }
+        }
+
+        // Compte total pour la pagination
+        $totalRecords = count($queryBuilder->getQuery()->getResult());
+        $totalPages = ceil($totalRecords / $limit);
+
+        // Ajout de la pagination
+        $users = $queryBuilder
+            ->setFirstResult($offset)
+            ->setMaxResults($limit)
+            ->getQuery()
+            ->getResult();
 
         return $this->render('user/index.html.twig', [
             'users' => $users,
-            'total_records' => $totalRecords,
-            'total_pages' => $totalPages,
             'current_page' => $page,
-            'search_term' => $searchTerm,
+            'total_pages' => $totalPages,
+            'total_records' => $totalRecords
         ]);
     }
 
